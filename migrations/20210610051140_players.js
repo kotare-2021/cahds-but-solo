5e0851ba3c293bae2645e8c67647d73c2465327a
Anastasia Gladun <anastasiagladoun@gmail.com>
2021-06-10 17:27:16 +1200

    fix table player

--- migrations/20210610051140_players.js
+++ migrations/20210610051140_players.js
@@ -1,11 +1,11 @@
 exports.up = function(knex) {
-    return knex.schema.createTable('players', (table) => {
+    return knex.schema.createTable('player', (table) => {
         table.increments('id').primary();
         table.string('name');
       })
 };
 
 exports.down = function(knex) {
-    return knex.schema.dropTable('players')
+    return knex.schema.dropTable('player')
 };
 
