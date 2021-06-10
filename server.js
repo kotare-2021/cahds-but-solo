714f0eb924a4fbd9616566eedeecbfe2e34271cd
lucas-f0ster <lucasf284@gmail.com>
2021-06-10 16:45:06 +1200

    generate basic file structure template

--- server.js
+++ server.js
@@ -1,2 +1,18 @@
 const express = require('express')
 const hbs = require('express-handlebars')
+
+const server = express()
+
+// Middleware
+
+server.engine('hbs', hbs({ extname: 'hbs' }))
+server.set('view engine', 'hbs')
+server.use(express.urlencoded({ extended: true }))
+
+// Routes
+
+server.get('/', (req, res) => {
+  res.send('Hi!')
+})
+
+module.exports = server
