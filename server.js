803806be9a5333d71df97c9931e706c2547ca5b1
lucas-f0ster <lucasf284@gmail.com>
2021-06-11 11:53:07 +1200

    unify file versions

--- server.js
+++ server.js
@@ -15,30 +15,6 @@ server.use(express.static('public'))
 
 
 
-// 1st stage - setup
- - assign 5 cards the player_id 1
- - grab all question
- - set round to 1
-
-
-let gameData = {
-  round: 1,
-  handCards: [
-    {id: 1, answer: 'hi', round_id: 1},
-    {id: 2, answer: 'hi', round_id: 500},
-    {id: 3, answer: 'hi', round_id: 2},
-    {id: 4, answer: 'hi', round_id: 500},
-    {id: 5, answer: 'hi', round_id: 500}
-  ],
-  questionCard: [
-    {id: 1, questions: 'hi', round_id: 1},
-    {id: 2, questions: 'hi', round_id: 500},
-    {id: 3, questions: 'hi', round_id: 2},
-    {id: 4, questions: 'hi', round_id: 500},
-    {id: 5, questions: 'hi', round_id: 500}
-  ]
-
-}
 
 server.get('/', (req, res) => {
   res.render('home', {cards: [{answer: 'hi'}, {answer: 'hi'}, {answer: 'hi'}, {answer: 'hi'}, {answer: 'hi'}]})
