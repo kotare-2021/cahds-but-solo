25a42ebd00b9c8eb77d13e8c72ba17b0602a6708
lucas-f0ster <lucasf284@gmail.com>
2021-06-10 17:24:13 +1200

    create temp db func

--- db.js
+++ db.js
@@ -9,3 +9,14 @@ module.exports = {
 const testFunc = () => {
   return null
 }
+
+const getAnswerCardById = (id, db = connection) => {
+  return db('users').select()
+    .where('id', id)
+    .then(resp => {
+      return resp
+    })
+    .catch(err => {
+      console.log(err.message)
+    })
+}
