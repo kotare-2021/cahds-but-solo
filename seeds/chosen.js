43463545f8b3491f943cb04e97da836d319bd31d
lucas-f0ster <lucasf284@gmail.com>
2021-06-11 15:01:37 +1200

    lll

--- seeds/chosen.js
+++ seeds/chosen.js
@@ -5,21 +5,21 @@ exports.seed = function(knex) {
     .then(function () {
       // Inserts seed entries
       return knex('answersCards').insert([
-        {id: 1, answer: 'Active listening',round_id: 500, player_id: 1,chosen: 'false' },
-        {id: 2, answer: 'Justin Bieber',round_id: 500, player_id: 0, chosen: 'false'},
-        {id: 3, answer: 'Shiny objects',round_id: 500, player_id: 0, chosen: 'false' },
-        {id: 4, answer: 'Bees?',round_id: 500, player_id: 1, chosen: 'false' },
-        {id: 5, answer: 'A little boy who wont shut the fuck up about dinosaurs',round_id: 500, player_id: 0, chosen: 'false' },
-        {id: 6, answer: 'Daddy issues',round_id: 500, player_id: 0, chosen: 'false' },
-        {id: 7, answer: 'Your weird brother',round_id: 500, player_id: 1, chosen: 'false' },
-        {id: 8, answer: 'Mansplaining',round_id: 500, player_id: 0, chosen: 'false' },
-        {id: 9, answer: 'Fear itself',round_id: 500, player_id: 0, chosen: 'false' },
-        {id: 10, answer: 'Old-people smell',round_id: 500, player_id: 0,chosen: 'false' },
-        {id: 11, answer: 'Knife crime',round_id: 500, player_id: 1, chosen: 'false' },
-        {id: 12, answer: 'The economy',round_id: 500, player_id: 0, chosen: 'false' },
-        {id: 13, answer: 'Being a dinosaur',round_id: 500, player_id: 0, chosen: 'false' },
-        {id: 14, answer: 'Positive feedback',round_id: 500, player_id: 1, chosen: 'false' },
-        {id: 15, answer: 'My check in',round_id: 500, player_id: 0, chosen: 'false' },
+        {id: 1, answer: 'Active listening',round_id: 500, player_id: 1,chosen: false },
+        {id: 2, answer: 'Justin Bieber',round_id: 500, player_id: 0, chosen: false},
+        {id: 3, answer: 'Shiny objects',round_id: 500, player_id: 0, chosen: false },
+        {id: 4, answer: 'Bees?',round_id: 500, player_id: 1, chosen: false },
+        {id: 5, answer: 'A little boy who wont shut the fuck up about dinosaurs',round_id: 500, player_id: 0, chosen: false },
+        {id: 6, answer: 'Daddy issues',round_id: 500, player_id: 0, chosen: false },
+        {id: 7, answer: 'Your weird brother',round_id: 500, player_id: 1, chosen: false },
+        {id: 8, answer: 'Mansplaining',round_id: 500, player_id: 0, chosen: false },
+        {id: 9, answer: 'Fear itself',round_id: 500, player_id: 0, chosen: false },
+        {id: 10, answer: 'Old-people smell',round_id: 500, player_id: 0,chosen: false },
+        {id: 11, answer: 'Knife crime',round_id: 500, player_id: 1, chosen: false },
+        {id: 12, answer: 'The economy',round_id: 500, player_id: 0, chosen: false },
+        {id: 13, answer: 'Being a dinosaur',round_id: 500, player_id: 0, chosen: false },
+        {id: 14, answer: 'Positive feedback',round_id: 500, player_id: 1, chosen: false },
+        {id: 15, answer: 'My check in',round_id: 500, player_id: 0, chosen: false },
       ]);
     });
 };
--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const hbs = require('express-handlebars')
-const { isNull } = require('util')
 const db = require('./db.js')
 
 const server = express()
@@ -16,41 +15,57 @@ server.use(express.static('public'))
 gameData = {}
 
 server.get('/', (req, res) => {
-  res.render('startPage')
+  console.log('/ gt=et===')
+  // db.reset()
+  // .then(() => {
+    res.render('startPage')
+  // })
 })
 
-server.get('/play/:id', (req, res) => {
-  res.render('home', gameData)
-})
 
 server.post('/play/:id', (req, res) => {
+  console.log('post route id', req.params.id)
   if(gameData.round < 5){
     gameData.round ++
     return db.chooseAnswer(req.params.id)
-    .then(res => {
-      return db.getHand()
-        .then(resp => {
-          gameData.hand = resp
-          console.log(gameData.round)
-          res.render('round1', gameData)
-        })
-    })
-  }else{
+      .then(() => {
+        gameData.hand.forEach(e => {
+          if(!e.chosen) {
+            delete e.chosen
+          }
+          console.log('hand', e)
+                //       return null
+              })
+              res.render('round1', gameData)
+              // res.send('yes')
+      })
+      .catch(err => {
+        console.log('catch')
+        res.send(err.message)
+      })
+  } else {
+    console.log('else')
     res.redirect('/done')
   }
 })
 
+server.get('/play/:id', (req, res) => {
+  console.log('/play/id get')
+  res.render('home', gameData)
+})
+
 server.post('/', (req, res) => {
+  console.log('/ post')
   // let gameData = {}
   gameData.round = 1
   return db.getHand()
   .then(resp => {
     gameData.hand = resp
-    console.log(resp)
+    // console.log(resp)
     return db.currentQ(1)
     .then(resp => {
       gameData.question = resp
-      console.log(gameData)
+      // console.log(gameData)
       res.render('home', gameData)
     })
   })
